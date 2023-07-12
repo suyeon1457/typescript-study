cdf2d20fc1be77fdb3d5a0a05a14600982440f94
suyeon1457 <smb1457@gmail.com>
2023-07-13 02:06:37 +0900

    feat: 연습문제 1번 풀이
    - tsconfig.json 재생성

--- Programming-TypeScript/chapter-2/src/index.ts
+++ Programming-TypeScript/chapter-2/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello TypeScript!')
+// console.log('Hello TypeScript!')
+
+// exercise 1
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b,
+};
+let d = c.apple * 4;
+// 유효하지 않은 코드를 구현하면 빨간 밑줄 표시가 나타남(Typeerror)
+// let d2 = [3] * 4
